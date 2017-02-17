4751e7bea6df2cef650e167ca06773c0c31f508e
Bryce <bthomsen@spscommerce.com>
2017-02-17 11:57:48 -0600

    docker run updates; removed readme and old alias script

--- setup_alias.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-for i in ./*; do
-  if [ -d "$i" ];
-    then
-      echo "alias $(basename "$i")='docker pull brycetho/stevedore:$(basename "$i"); docker run -it --rm brycetho/stevedore:$(basename "$i")'" >> .bash_profile;
-  fi;
-done
