d15115ad4fd5a24700146d40f7f86f44ade0e87f
Dan Farrell <dfarrell@spscommerce.com>
2017-02-20 14:58:04 -0600

    bash updates

--- alias.sh
+++ alias.sh
@@ -1,12 +1,13 @@
 #!/bin/bash
 # Check every dir for an "alias" at the top of each Dockerfile
-for i in ./*; do
-  if [ -d "$i" ];
-    then
-      DALIAS=`head -n 1 $(basename "$i")/Dockerfile | awk -F'# ' '{print $2}'`;
+echo -n '' > .bash_profile_alias.new
+for i in `find . -type f -iname 'Dockerfile*'`: do
+    DALIAS=`head -n 1 "$i" | awk -F'# ' '{print $2}'`;
       if [ "$DALIAS" ];
         then
-          echo $DALIAS  >> .bash_profile;
+          echo $DALIAS  >> .bash_profile_alias.new;
       fi;
-  fi;
 done
+# could check size and add/remove instead of just adding
+mv .bash_profile_alias.new .bash_profile_alias
+grep -q -F 'source .bash_profile_alias' || echo -e '\nsource .bash_profile_alias' >> .bash_profile
